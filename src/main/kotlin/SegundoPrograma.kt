14a232ce5104dee8d6fa9f70bf0c9d2ce29ba39b
davioliveira17 <davioliveira100223@gmail.com>
2023-04-06 20:20:57 -0300

    primeiro versionamento do projeto app segundo

---
+++ src/main/kotlin/SegundoPrograma.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+ var variavelA = "Cidade"
+ var variavelB = "Natal"
+
+ imprimir(variavelA,variavelB)
+
+ imprimir(variavelB,variavelA)
+
+}
+
+fun imprimir(variavelA:String, variavelB:String){
+
+ print("$variavelA\n$variavelB\n")
+
+}
+
+
